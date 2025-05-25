abda3d8eaa11f1a4b82b1f8ca026824b4aec4a54
abranches3318 <gabrielnpc2010@hotmail.com>
2025-05-25 01:22:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ campoBusca.addEventListener('focus', () => {
 campoBusca.addEventListener('blur', () => {
     campoBusca.placeholder = 'Buscar';
 });
+
+const esquerda = document.querySelector('.seta.esquerda');
+const direita = document.querySelector('.seta.direita');
+const cards = document.querySelector('.cards');
+
+esquerda.addEventListener('click', () => {
+    cards.scrollBy({ left: -300, behavior: 'smooth' });
+});
+
+direita.addEventListener('click', () => {
+    cards.scrollBy({ left: 300, behavior: 'smooth' });
+});
