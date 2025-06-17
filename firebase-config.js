1e53a56db66f11c713943878a6cf7ac824929696
abranches3318 <gabrielnpc2010@hotmail.com>
2025-06-16 23:19:32 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,8 @@
+// js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 const firebaseConfig = {
 
   apiKey: "AIzaSyDhjUescYhrZ1e12M6nv5mnWxDovNcGxw0",
@@ -17,3 +22,8 @@ const firebaseConfig = {
   measurementId: "G-5VS0S34GGN"
 
 };
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
