18f8633fe6a1ab640fb5eb60000a5703abc1f9f5
abranches3318 <gabrielnpc2010@hotmail.com>
2025-06-16 23:11:49 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDhjUescYhrZ1e12M6nv5mnWxDovNcGxw0",
+
+  authDomain: "clickserra-anuncios.firebaseapp.com",
+
+  databaseURL: "https://clickserra-anuncios-default-rtdb.firebaseio.com",
+
+  projectId: "clickserra-anuncios",
+
+  storageBucket: "clickserra-anuncios.firebasestorage.app",
+
+  messagingSenderId: "251868045964",
+
+  appId: "1:251868045964:web:34f527f3d7c380746211a9",
+
+  measurementId: "G-5VS0S34GGN"
+
+};
