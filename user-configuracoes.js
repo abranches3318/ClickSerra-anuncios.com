dbd80d28165b0155451bd2a2d483d94647bd71ec
abranches3318 <gabrielnpc2010@hotmail.com>
2025-07-04 21:43:41 -0300

    Update user-configuracoes.js

--- user-configuracoes.js
+++ user-configuracoes.js
@@ -28,7 +28,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDhjUescYhrZ1e12M6nv5mnWxDovNcGxw0",
   authDomain: "clickserra-anuncios.firebaseapp.com",
   projectId: "clickserra-anuncios",
-  storageBucket: "clickserra-anuncios.appspot.com",
+  storageBucket: "clickserra-anuncios.firebasestorage.app",
   messagingSenderId: "251868045964",
   appId: "1:251868045964:web:34f527f3d7c380746211a9"
 };
