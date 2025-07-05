7ced5af4001fb63a8255a580f89282dc80815f57
abranches3318 <gabrielnpc2010@hotmail.com>
2025-07-05 01:01:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-const db = firebase.firestore();
+
 
 // Menu hambúrguer
 const botaoMenu = document.getElementById('botaoMenu');
