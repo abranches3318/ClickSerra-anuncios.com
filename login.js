518416d190a5c7f2df7eef6fa2bedf84652e8b21
abranches3318 <gabrielnpc2010@hotmail.com>
2025-07-12 21:43:45 -0300

    Update login.js

--- login.js
+++ login.js
@@ -5,15 +5,14 @@ let confirmationResult = null;
 
 // === Inicializar Firebase ===
 function inicializarFirebase() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyDhjUescYhrZ1e12M6nv5mnWxDovNcGxw0",
-    authDomain: "clickserra-anuncios.firebaseapp.com",
-    databaseURL: "https://clickserra-anuncios-default-rtdb.firebaseio.com",
-    projectId: "clickserra-anuncios",
-    storageBucket: "clickserra-anuncios.appspot.com",
-    messagingSenderId: "251868045964",
-    appId: "1:251868045964:web:34f527f3d7c380746211a9"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDhjUescYhrZ1e12M6nv5mnWxDovNcGxw0",
+  authDomain: "clickserra-anuncios.firebaseapp.com",
+  projectId: "clickserra-anuncios",
+  storageBucket: "clickserra-anuncios.firebasestorage.app",
+  messagingSenderId: "251868045964",
+  appId: "1:251868045964:web:34f527f3d7c380746211a9"
+};
   app = firebase.initializeApp(firebaseConfig);
   auth = firebase.auth();
 }
